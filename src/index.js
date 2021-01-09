91db0b9537a2a0e53f8552dc07c2730062646003
Nishant Ketu <69218488+ketunishant@users.noreply.github.com>
2021-01-09 23:22:58 +0530

    use of useState

--- src/index.js
+++ src/index.js
@@ -1,50 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-
+import App from "./App.js";
 //WHEN START CODESANDBOX AUTOMATICALLY SHOW
 //NOW START //
 //LETS GO
 
-let count = 0;
-
-function increase() {
-  count++;
-  ReactDOM.render(
-    <div className="App">
-      <h2>THINK WITH CODE</h2>
-      <h1>{count}</h1>
-
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
-    </div>,
-    document.getElementById("root")
-  );
-}
-
-function decrease() {
-  count--;
-  ReactDOM.render(
-    <div className="App">
-      <h2>THINK WITH CODE</h2>
-      <h1>{count}</h1>
-
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
-    </div>,
-    document.getElementById("root")
-  );
-}
-
+const insta = "https://www.instagram.com/think_with_code/";
 ReactDOM.render(
   <div className="App">
-    <h2>THINK WITH CODE</h2>
-    <h1>{count}</h1>
-
-    <button onClick={increase}>+</button>
-    <button onClick={decrease}>-</button>
+    <h2>
+      <a href={insta}>THINK WITH CODE</a>
+    </h2>
+    <App />
   </div>,
   document.getElementById("root")
 );
-
-//THANKS FOR WATCHING
